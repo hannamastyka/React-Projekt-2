440417f65cbd45d5089f58a0b0cff8863d5ded51
hannamastyka <hanna.mastyka@outlook.com>
2023-10-14 13:23:55 +0200

    zadaniezadaniejedendwa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState("0.00");
+  const calculateValue = (event) => {
+    event.preventDefault();
+    const value = event.currentTarget.elements.amount.value;
+
+    const rate = 4.5793;
+    setResult((value * rate).toFixed(2));
+    fetch(
+      "https://api.nbp.pl/api/exchangerates/rates/a/EUR/?format=json"
+    ).catch(() => SpeechSynthesisErrorEvent("Bład serwera"));
+  };
 
   return (
     <div>
-      <from>
+      <from onSumbit={calculateValue}>
         <label htmFor="amount">Kwota:</label>
-        <inout type="number" id="amount" min="0.01" step="0.01" required />
+        <input type="number" id="amount" min="0.01" step="0.01" required />
         <button type="submit">Przelicz</button>
       </from>
+      <p>Wynik po przeliczeniu: {result}</p>
     </div>
   );
 }
